665acc5783194f09b0350428956baa4685707c75
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-06-23 11:56:03 -0400

    add seed file, schema, and inquirer prompts

--- db/tracker_schema.sql
+++ db/tracker_schema.sql
@@ -11,15 +11,15 @@ CREATE TABLE tracker_employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT
+  role_id INT FOREIGN KEY (tracker_role.id),
+  manager_id INT FOREIGN KEY (tracker_role.title)
 );
 
 CREATE TABLE tracker_role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT FOREIGN KEY (tracker_department.id)
 );
 
 CREATE TABLE tracker_department (
--- server.js
+++ server.js
@@ -1,21 +1,98 @@
-const express = require('express');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-// Import the connection object
-const sequelize = require('./config/connection');
-
-const app = express();
-const PORT = process.env.PORT || 3030;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Connect to the database before starting the Express.js server
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Kitlinger12805',
+  database: 'tracker_db'
 });
 
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`App is listening`);  
+  runStart();
+});
 
+const runStart = () => {
+  inquirer
+    .prompt({
+      name: 'employees',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'View All Employees By Department',
+        'View All Employees by Manager',
+        'Add Employee',
+        'Remove Employee',
+        'Update Employee Role',
+        'Update Employee Manager'
+      ],
+    })
+    .then((answers) => {
+      switch (answers.employees) {
+        case 'View All Employees':
+          runEmployees();
+          break;
+        case 'View All Employees By Department':
+          runByDepartment();
+          break;
+        case 'View All Employees by Manager':
+          runByManager();
+          break;
+        case 'Add Employee':
+          runAddEmployee();
+          break;
+        case 'Remove Employee':
+          runRemoveEmployee();
+          break;
+        case 'Update Employee Role':
+          runUpdateRole();
+          break;
+        case 'Update Employee Manager':
+          runUpdateManager();
+          break;
+          
+        default:
+          console.log(`Invalid action: ${answers.employees}`);
+          break;
+      }
+    });
+};
 
+const runEmployees = () => {
+  console.log('runEmployees()');
+  runStart();
+};
 
+const runByDepartment = () => {
+  console.log('runByDepartment()');
+  runStart();
+};
 
+const runByManager = () => {
+  console.log('runByManager()');
+  runStart();
+};
 
+const runAddEmployee = () => {
+  console.log('runAddEmployee()');
+  runStart();
+};
+
+const runRemoveEmployee = () => {
+  console.log('runRemoveEmployee()');
+  runStart();
+};
+
+const runUpdateRole = () => {
+  console.log('runUpdateRole()');
+  runStart();
+};
+
+const runUpdateManager = () => {
+  console.log('runUpdateManager()');
+  runStart();
+};
