0074f42aed4093b66b3ba3e2485f691cf1ed6fc7
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-06-23 01:14:35 -0400

    server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const routes = require('./routes');
+
+// Import the connection object
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -8,10 +9,13 @@ const PORT = process.env.PORT || 3030;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+// Connect to the database before starting the Express.js server
+sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
+
+
+
+
+
+
