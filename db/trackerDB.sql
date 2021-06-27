8f923aaaa6eb784abed9cde19081761748a71ac5
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-06-27 17:19:25 -0400

    final code

--- db/trackerDB.sql
+++ db/trackerDB.sql
@@ -19,6 +19,7 @@ CREATE TABLE role (
   department_id INT NOT NULL,
   FOREIGN KEY (department_id)
     REFERENCES department(id)
+  ON DELETE CASCADE,
 );
 
 CREATE TABLE employee (
@@ -29,7 +30,9 @@ CREATE TABLE employee (
   manager_id INT NULL,
   FOREIGN KEY (role_id)
     REFERENCES role(id)
+  ON DELETE CASCADE
   FOREIGN KEY (manager_id)
     REFERENCES (id)
+  ON DELETE CASCADE
 );
 
--- index.js
+++ index.js
@@ -31,6 +31,8 @@ const runStart = () => {
         'Add a new Employee Role',
         'Add a new Employee',
         'Remove Employee',
+        'Remove Employee Role',
+        'Remove a Department',
         'Update Employee Role',
         'Update Employee Manager',
         'EXIT'
@@ -64,6 +66,12 @@ const runStart = () => {
           break;
         case 'Remove Employee':
           runRemoveEmployee();
+          break;
+        case 'Remove Employee Role':
+          runRemoveRole();
+          break;
+        case 'Remove a Department':
+          runRemoveDept();
           break;
         case 'Update Employee Role':
           runUpdateRole();
@@ -136,14 +144,112 @@ const runEmployees = () => {
 };
 
 const runByDepartment = () => {
-  console.log('runByDepartment()');
-  runStart();
-};
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'department',
+      message: 'Choose a department to see its employees:',
+      choices: () => {
+        let deptArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT name, id FROM department`,
+          (err, res) => {
+            if (err) throw err;
+            res.forEach((department) => {
+              deptArray.push({
+                name: department.name, 
+                value: department.id
+              });
+            });
+            resolve(deptArray);
+          });
+        });
+      }
+    },
+
+  ]).then((answer) => {
+    const byDepartment = `SELECT 
+    employee.id AS "Employee ID", 
+      employee.first_name AS "First Name", 
+      employee.last_name AS "Last Name", 
+      role.title AS "Title", 
+      department.name AS "Department", 
+      role.salary AS "Starting Salary", 
+      CONCAT(manager.first_name, " ", manager.last_name) AS "Employee's Manager"
+      FROM employee
+      LEFT JOIN role
+        ON role.id = employee.role_id
+      LEFT JOIN department
+        ON department.id = role.department_id
+      LEFT JOIN employee AS manager
+        ON manager.id = employee.manager_id
+        WHERE department.id = ${answer.department};`;
+
+    connection.query(byDepartment, (err, res) => {
+      if (err) throw err;
+      if (res.length == 0) {
+        console.log('This department does not have any employees.\n')
+      } else {
+        console.table(res);
+      }
+      runStart();
+    });
+  });
+}
 
 const runByManager = () => {
-  console.log('runByManager()');
-  runStart();
-};
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'manager',
+      message: 'Choose a manager to see his or her employees:',
+      choices: () => {
+        let employeeArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT CONCAT(first_name, " ", last_name) AS name, id FROM employee`,
+          (err, res) => {
+            if (err) throw err;
+            res.forEach((manager) => {
+              employeeArray.push({
+                name: manager.name, 
+                value: manager.id
+              });
+            });
+            resolve(employeeArray);
+          });
+        });
+      }
+    },
+
+  ]).then((answer) => {
+    const byManager = `SELECT 
+    employee.id AS "Employee ID", 
+      employee.first_name AS "First Name", 
+      employee.last_name AS "Last Name", 
+      role.title AS "Title", 
+      department.name AS "Department", 
+      role.salary AS "Starting Salary", 
+      CONCAT(manager.first_name, " ", manager.last_name) AS "Employee's Manager"
+      FROM employee
+      LEFT JOIN role
+        ON role.id = employee.role_id
+      LEFT JOIN department
+        ON department.id = role.department_id
+      LEFT JOIN employee AS manager
+        ON manager.id = employee.manager_id
+        WHERE employee.manager_id = ${answer.manager};`;
+
+    connection.query(byManager, (err, res) => {
+      if (err) throw err;
+      if (res.length == 0) {
+        console.log('This employee does not have managment duties.\n')
+      } else {
+        console.table(res);
+      }
+      runStart();
+    });
+  });
+}
 
 const runAddDepartment = () => {
   inquirer.prompt([
@@ -185,7 +291,10 @@ const runAddRole = () => {
           (err, res) => {
             if (err) throw err;
             res.forEach((department) => {
-              deptArray.push({name: department.name, value: department.id});
+              deptArray.push({
+                name: department.name, 
+                value: department.id
+              });
             });
             resolve(deptArray);
           });
@@ -239,13 +348,16 @@ const runAddEmployee = () => {
       name: 'manager',
       message: 'Who is the manager for the new employee?',
       choices: () => {
-        let managerArray = [];
+        let managerArray = [{name: '', value: null}];
         return new Promise((resolve, reject) => {
           connection.query(`SELECT CONCAT(first_name, " ", last_name) AS name, id FROM employee`,
           (err, res) => {
             if (err) throw err;
-            res.forEach((man) => {
-              managerArray.push({name: man.name, value: man.id});
+            res.forEach((manager) => {
+              managerArray.push({
+                name: manager.name, 
+                value: manager.id
+              });
             });
             resolve(managerArray);
           });
@@ -266,16 +378,212 @@ const runAddEmployee = () => {
 };
 
 const runRemoveEmployee = () => {
-  console.log('runRemoveEmployee()');
-  runStart();
-};
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'empRemove',
+      message: 'Select an employee to remove them from the employee list:',
+      choices: () => {
+        let listArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT CONCAT(first_name, ' ', last_name) AS name, id FROM employee;`,
+            (err, res) => {
+              if (err) throw err;
+              res.forEach((employee) => {
+                listArray.push({
+                  name: employee.name, 
+                  value: employee.id 
+                });
+              });
+              resolve(listArray);
+            });
+        });
+      }
+    },
+  ]).then((answer) => {
+    connection.query(`DELETE FROM employee WHERE id = ${answer.empRemove}`,
+      (err, res) => {
+        if (err) throw err;
+        console.log('Selected employee file was deleted from the system.\n');
+        runStart();
+    });
+  });
+}
+
+const runRemoveRole = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'removeRole',
+      message: 'Select an employee role to delete from the database:',
+      choices: () => {
+        let roleArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT title, id FROM role;`,
+            (err, res) => {
+              if (err) throw err;
+              res.forEach((role) => {
+                roleArray.push({
+                  name: role.title, 
+                  value: role.id 
+                });
+              });
+              resolve(roleArray);
+            });
+        });
+      }
+    },
+  ]).then((answer) => {
+    connection.query(`DELETE FROM role WHERE id = ${answer.removeRole}`,
+      (err, res) => {
+        if (err) throw err;
+        console.log('Selected employee role was deleted from the system.\n');
+        runStart();
+    });
+  });
+}
+
+const runRemoveDept = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'deptRemove',
+      message: 'Select an department to remove it from the database:',
+      choices: () => {
+        let deptArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT name, id FROM department;`,
+            (err, res) => {
+              if (err) throw err;
+              res.forEach((dept) => {
+                deptArray.push({
+                  name: dept.name, 
+                  value: dept.id 
+                });
+              });
+              resolve(deptArray);
+            });
+        });
+      }
+    },
+  ]).then((answer) => {
+    connection.query(`DELETE FROM department WHERE id = ${answer.deptRemove}`,
+      (err, res) => {
+        if (err) throw err;
+        console.log('Selected department was deleted from the system.\n');
+        runStart();
+    });
+  });
+}
+
+
+
+
 
 const runUpdateRole = () => {
-  console.log('runUpdateRole()');
-  runStart();
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'emp',
+      message: 'Select employee to update thier role:',
+      choices: () => {
+        let listArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT CONCAT(first_name, ' ', last_name) AS name, id FROM employee;`,
+            (err, res) => {
+              if (err) throw err;
+              res.forEach((employee) => {
+                listArray.push({
+                  name: employee.name, 
+                  value: employee.id 
+                });
+              });
+              resolve(listArray);
+            });
+        });
+      }
+    },
+    {
+      type: 'list',
+      name: 'empRole',
+      message: 'Select new role for this employee:',
+      choices: () => {
+        let empRoleArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT title, id FROM role;`,
+          (err, res) => {
+            if (err) throw err;
+            res.forEach((role) => {
+              empRoleArray.push({
+                name: role.title, 
+                value: role.id
+              });
+            });
+            resolve(empRoleArray);
+          });
+        });
+      }
+    }
+  ]).then((answer) => {
+    connection.query(`UPDATE employee SET role_id = ${answer.empRole} WHERE id = ${answer.emp};`,
+      (err, res) => {
+        if (err) throw err;
+        console.log('New role for chosen employee has been updated.\n');
+        runStart();
+      });
+  });
 };
 
 const runUpdateManager = () => {
-  console.log('runUpdateManager()');
-  runStart();
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'emp3',
+      message: 'Select employee to update who they report to:',
+      choices: () => {
+        let listArray = [];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT CONCAT(first_name, ' ', last_name) AS name, id FROM employee;`,
+            (err, res) => {
+              if (err) throw err;
+              res.forEach((employee) => {
+                listArray.push({
+                  name: employee.name, 
+                  value: employee.id 
+                });
+              });
+              resolve(listArray);
+            });
+        });
+      }
+    },
+    {
+      type: 'list',
+      name: 'newManager',
+      message: 'Select who the new manager for this employee should be:',
+      choices: () => {
+        let newManArray = [{name: '', value: null}];
+        return new Promise((resolve, reject) => {
+          connection.query(`SELECT CONCAT(first_name, ' ', last_name) AS name, id FROM employee;`,
+          (err, res) => {
+            if (err) throw err;
+            res.forEach((newManager) => {
+              newManArray.push({
+                name: newManager.name, 
+                value: newManager.id
+              });
+            });
+            resolve(newManArray);
+          });
+        });
+      }
+    }
+  ]).then((answer) => {
+    connection.query(`UPDATE employee SET manager_id = ${answer.newManager} WHERE id = ${answer.emp3};`,
+      (err, res) => {
+        if (err) throw err;
+        console.log('New manager for chosen employee has been updated.\n');
+        runStart();
+      });
+  });
 };
